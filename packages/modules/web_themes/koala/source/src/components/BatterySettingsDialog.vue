9d8ad46c152ed328a0ba4a989a1cb9078172d216
BrettS <brett.scott@openwb.de>
2026-03-17 11:11:05 +0100

    format tittle and range slider labels

--- packages/modules/web_themes/koala/source/src/components/BatterySettingsDialog.vue
+++ packages/modules/web_themes/koala/source/src/components/BatterySettingsDialog.vue
@@ -4,14 +4,23 @@
     :maximized="isSmallScreen"
     :backdrop-filter="isSmallScreen ? '' : 'blur(4px)'"
   >
-    <q-card>
-      {{ batteryRange }}
+    <q-card class="card-width">
       <q-card-section>
+        <!-- {{ batteryRange }} -->
         <div class="row no-wrap">
-          <div class="text-h6 q-pr-sm">Speicher-Beachtung:</div>
-          <div class="text-h6 ellipsis" :title="name">{{ name }}</div>
+          <div>
+            <div class="text-h6 q-pr-sm">Speicher-Beachtung:</div>
+            <div class="text-h6 ellipsis" :title="name">{{ name }}</div>
+          </div>
           <q-space />
-          <q-btn icon="close" flat round dense v-close-popup />
+          <q-btn
+            icon="close"
+            flat
+            round
+            dense
+            v-close-popup
+            class="close-btn"
+          />
         </div>
       </q-card-section>
       <q-separator />
@@ -20,15 +29,16 @@
         <BatteryModeButtons />
       </q-card-section>
       <q-card-section v-if="batteryMode === 'min_soc_bat_mode'">
-          <div class="text-subtitle2">SoC-Bereich des Speichers:</div>
-          <q-range
-            v-model="batteryRange"
-            :min="0"
-            :max="100"
-            :step="1"
-            :markers="10"
-            label
-          />
+        <div class="text-subtitle2">SoC-Bereich des Speichers % :</div>
+        <q-range
+          v-model="batteryRange"
+          :min="0"
+          :max="100"
+          :step="1"
+          :markers="10"
+          label
+          label-always
+        />
       </q-card-section>
     </q-card>
   </q-dialog>
@@ -70,3 +80,22 @@ const batteryRange = computed({
   },
 });
 </script>
+<style lang="scss" scoped>
+.card-width {
+  max-width: 26em;
+}
+.close-btn {
+  height: 2.5em;
+  width: 2.5em;
+}
+:deep(.q-slider__pin) {
+  top: 100%;
+  transform: scaleY(-1);
+}
+:deep(.q-slider__text-container) {
+  transform: scaleY(-1) !important;
+}
+:deep(.q-range) {
+  padding-bottom: 24px;
+}
+</style>
